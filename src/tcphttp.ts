f4fd7908459a01705bb63566125d8909c13d6360
Ryo Ota <nwtgck@gmail.com>
2018-11-19 21:37:51 +0900

    [change] Enable CORS

--- src/tcphttp.ts
+++ src/tcphttp.ts
@@ -2,9 +2,15 @@ import * as net  from "net";
 import * as http from "http";
 
 // Connection command example: curl -H "X-HOST: example.com" -H "X-PORT: 80" --data-binary @req.txt localhost:8181
-export const server = http.createServer((req: http.IncomingMessage, res: http.OutgoingMessage)=>{
+export const server = http.createServer((req: http.IncomingMessage, res: http.ServerResponse)=>{
   const hostHeader = req.headers['x-host'];
   const portHeader = req.headers['x-port'];
+
+  // Enable CORS
+  res.writeHead(200, {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept, X-Host, X-Port"
+  });
 
   if (typeof hostHeader === "string" && typeof portHeader === "string" && portHeader.match(/^\d+$/)) {
     const host: string = hostHeader;
