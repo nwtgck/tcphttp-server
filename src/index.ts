9438931db67a680c44ed3ce0ee43e3de59108833
Ryo Ota <nwtgck@gmail.com>
2018-11-19 18:09:44 +0900

    [change] Add handling for invalid host and port

--- src/index.ts
+++ src/index.ts
@@ -7,27 +7,34 @@ const serverPort: number = 8181;
 
 // Connection command example: curl -H "X-HOST: example.com" -H "X-PORT: 80" --data-binary @req.txt localhost:8181 
 const server = http.createServer((req: http.IncomingMessage, res: http.OutgoingMessage)=>{
-  // TODO: Not to use `as`
-  const host: string = req.headers['x-host'] as string;
-  // TODO: Not to use `as`
-  const port: number = parseInt((req.headers['x-port'] as string) || "80");
+  const hostHeader = req.headers['x-host'];
+  const portHeader = req.headers['x-port'];
 
-  const client = new net.Socket();
-  client.connect(port, host, ()=>{
-    console.log("on connection");
-    req.on("data", (data)=>{
-      client.write(data);
+  if (typeof hostHeader === "string" && typeof portHeader === "string" && portHeader.match(/^\d+$/)) {
+    const host: string = hostHeader;
+    const port: number = parseInt(portHeader);
+
+    const client = new net.Socket();
+    client.connect(port, host, ()=>{
+      console.log("on connection");
+      req.on("data", (data)=>{
+        client.write(data);
+      });
     });
-  });
 
-  client.on('data', (data: Buffer)=>{
-    res.write(data);
-  });
+    client.on('data', (data: Buffer)=>{
+      res.write(data);
+    });
 
-  client.on('close', ()=>{
-    console.log('on close');
+    client.on('close', ()=>{
+      console.log('on close');
+      res.end();
+    });
+  } else {
+    console.log("Invalid host or port: ", hostHeader, portHeader);
+    // Close response because host or port are invalid
     res.end();
-  });
+  }
 });
 
 server.listen(serverPort, serverHost, ()=>{
