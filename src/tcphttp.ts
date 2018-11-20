ea7b0b4ca06318cda1591371e330be2aa37b97ab
Ryo Ota <nwtgck@gmail.com>
2018-11-20 11:16:39 +0900

    [change] Use query parameters to specify "host", "port", "tls" instead of HTTP headers

--- src/tcphttp.ts
+++ src/tcphttp.ts
@@ -1,6 +1,13 @@
 import * as net  from "net";
 import * as tls  from "tls";
 import * as http from "http";
+import * as url from "url";
+import {ParsedUrlQuery} from "querystring";
+import {opt, optMap} from "./utils";
+
+const HOST_PARAM_NAME = "host";
+const PORT_PARAM_NAME = "port";
+const TLS_PARAM_NAME  = "tls";
 
 /**
  * Convert bool string to boolean value
@@ -16,20 +23,23 @@ function boolStrToBool(boolStr: string | undefined): boolean {
 }
 
 export const server = http.createServer((req: http.IncomingMessage, res: http.ServerResponse)=>{
-  const hostHeader = req.headers['x-host'];
-  const portHeader = req.headers['x-port'];
-  const tlsHeader  = req.headers['x-tls'];
+  // Get query parameter
+  const query = opt(optMap(url.parse, req.url, true).query) as ParsedUrlQuery | undefined;
 
   // Enable CORS
   res.writeHead(200, {
     "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept, X-Host, X-Port, X-TLS"
+    "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept"
   });
 
-  if (typeof hostHeader === "string" && typeof portHeader === "string" && portHeader.match(/^\d+$/) && (tlsHeader === undefined || typeof tlsHeader === "string" && tlsHeader.match(/^(true|false)$/i))) {
-    const host: string     = hostHeader;
-    const port: number     = parseInt(portHeader);
-    const usesTls: boolean = boolStrToBool(tlsHeader);
+  if (query !== undefined &&
+      typeof query[HOST_PARAM_NAME] === "string" &&
+      typeof query[PORT_PARAM_NAME] === "string" && (query[PORT_PARAM_NAME] as string).match(/^\d+$/) &&
+      (query[TLS_PARAM_NAME] === undefined || typeof query[TLS_PARAM_NAME] === "string" && (query[TLS_PARAM_NAME] as string).match(/^(true|false)$/i))) {
+
+    const host: string     = query[HOST_PARAM_NAME] as string;
+    const port: number     = parseInt(query[PORT_PARAM_NAME] as string);
+    const usesTls: boolean = boolStrToBool(query[TLS_PARAM_NAME] as string | undefined);
 
 
     // Select connect-listener by `usersTls`
@@ -54,7 +64,7 @@ export const server = http.createServer((req: http.IncomingMessage, res: http.Se
       res.end();
     });
   } else {
-    console.log("Invalid host or port: ", hostHeader, portHeader);
+    console.log("Invalid query: ", query);
     // Close response because host or port are invalid
     res.end();
   }
